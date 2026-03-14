63893e6acf8af4d6ae8ff8ba50306483434a3ffe
Brad Spoerri <bwspoerri@gmail.com>
2026-03-14 10:56:19 -0400

    add second problem solution

---
+++ SP2_BradSpoerri.py
@@ -0,0 +1,37 @@
+# Brad Spoerri 
+# DS5010 - SP26
+# Problem Set 7 - Problem 2
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# question 2.1
+
+# read diamonds.csv into df
+diamonds = pd.read_csv('Files/diamonds.csv')
+
+# groupby cut then find count for each cut type
+cut_quality_dist = (diamonds.groupby(["cut"])["cut"]
+                    .value_counts(ascending=False))
+
+# plot bar chart with prescribed labelling and style
+plt.bar(cut_quality_dist.index, cut_quality_dist, width=0.6, color="grey")
+plt.xlabel('Cut Quality')
+plt.ylabel('Number of Diamonds')
+plt.title('Distribution of Diamonds by Cut')
+plt.show()
+
+# question 2.2
+
+# create df with just length
+length_freq = diamonds["x"]
+
+# plot histogram with prescribed styling; density param used for frequency
+plt.hist(length_freq, density=True, color="lightblue")
+plt.xlabel("Length (in mm)")
+plt.ylabel("Frequency")
+plt.title("Frequency Distribution of Diamonds by Length")
+# exclude bins with no values
+plt.xlim(3, 10) 
+plt.show()
